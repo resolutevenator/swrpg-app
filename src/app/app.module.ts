ee4b34bafaad96006a710e7e5a1a3776e3fe0363
Ryanasaurus <ryanasaurus97@gmail.com>
2019-01-01 22:33:51 +1300

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatCheckboxModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { GestureConfig } from '@angular/material';
+
+import 'hammerjs';
+
+declare var Hammer: any;
+@Injectable()
+export class HammerConfig extends GestureConfig  {
+  buildHammer(element: HTMLElement) {
+    return new GestureConfig({touchAction: 'pan-y'}).buildHammer(element);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +33,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    NoopAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: HammerConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
