16f6ea1ba693058dcb1b2b872130550faa769d52
Ryanasaurus <ryanasaurus97@gmail.com>
2019-01-04 21:29:48 +1300

    Added link to the race cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RaceBrowseComponent } from './race-browse/race-browse.component'
+import { RacesComponent } from './races/races.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: RaceBrowseComponent
+    component: RaceBrowseComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'race/:raceName',
+    component: RacesComponent,
   }
 ];
 
--- src/app/race-browse/race-browse.component.ts
+++ src/app/race-browse/race-browse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database/';
 
 @Component({
@@ -11,12 +12,19 @@ export class RaceBrowseComponent implements OnInit {
   races: any;
   _raceSubscription: any;
 
-  constructor(private db: AngularFireDatabase) {
+  constructor(
+    private router: Router,
+    private db: AngularFireDatabase
+    ) {
     this._raceSubscription = db.list('races/').valueChanges().subscribe(data => {
       this.races = data;
     }),(err) => {
       console.log('error: ', err)
     };
+  }
+
+  navigate(raceName: String) {
+    this.router.navigate(['/race', raceName.toLowerCase()])
   }
 
   ngOnInit() {
