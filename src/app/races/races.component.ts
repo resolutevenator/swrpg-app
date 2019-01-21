94e3bbef8bd708e81c0c7e6001ab9b3f2281009c
Ryanasaurus <ryanasaurus97@gmail.com>
2019-01-21 15:59:58 +1300

    added content to race page

--- src/app/races/races.component.ts
+++ src/app/races/races.component.ts
@@ -2,12 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatabaseService } from '../database.service';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-races',
   templateUrl: './races.component.html',
   styleUrls: ['./races.component.scss']
 })
+
 export class RacesComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 3, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
   public race: string;
 
