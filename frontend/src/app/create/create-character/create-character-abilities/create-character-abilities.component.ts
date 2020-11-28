c373edca97fd86d582a6cbe9ff08cc0f84bfc4d0
Ryanasaurus <ryanasaurus97@gmail.com>
2020-11-28 23:12:22 +1300

    abilities page update that allow the stats, need to align this with xp and actual stats

--- frontend/src/app/create/create-character/create-character-abilities/create-character-abilities.component.ts
+++ frontend/src/app/create/create-character/create-character-abilities/create-character-abilities.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCharacterAbilitiesComponent implements OnInit {
 
+  brawn: number = 1;
+  agility: number = 1;
+  intellect: number = 1;
+  cunning: number = 1;
+  willpower: number = 1;
+  presence: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  increaseStat(stat: string) {
+    switch (stat) {
+      case "b":
+        this.brawn++;
+        break;
+      case "a":
+        this.agility++;
+        break;
+      case "i":
+        this.intellect++;
+        break;
+      case "c":
+        this.cunning++;
+        break;
+      case "w":
+        this.willpower++;
+        break;
+      case "p":
+        this.presence++;
+        break;
+    }
+
+  }
+
+  decreaseStat(stat: string) {
+    switch (stat) {
+      case "b":
+        this.brawn--;
+        break;
+      case "a":
+        this.agility--;
+        break;
+      case "i":
+        this.intellect--;
+        break;
+      case "c":
+        this.cunning--;
+        break;
+      case "w":
+        this.willpower--;
+        break;
+      case "p":
+        this.presence--;
+        break;
+    }
+
   }
 
 }
